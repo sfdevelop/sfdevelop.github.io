f8ce81cac871a7be3a83cde0f0e9138f0d51eeed
sfdevelop <sfdevelop747@gmail.com>
2020-03-28 12:48:50 +0200

    28.03.2020
    Внесли правки относительно файла

--- granit/assets/js/main.js
+++ granit/assets/js/main.js
@@ -28257,3 +28257,23 @@ $(document).ready(function () {
 		}
 	}); //scroll 
 });
+
+//Прелоадер
+
+$(document).ready(function () {
+	var $preloader = $('#p_prldr'),
+		$svg_anm = $preloader.find('.svg_anm');
+	$svg_anm.fadeOut();
+	$preloader.delay(500).fadeOut('slow');
+});
+
+//віпадающее Мню при наведении
+$('.dropdown-toggle').click(function (e) {
+	if ($(document).width() > 768) {
+		e.preventDefault();
+		var url = $(this).attr('href');
+		if (url !== '#') {
+			window.location.href = url;
+		}
+	}
+});
